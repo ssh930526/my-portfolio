a09af6d696d96474eee0af8219b1a4dd02a2f1fc
Sihui Shen <sihui@Sihuis-MacBook-Pro.local>
2021-08-07 16:29:04 -0500

    wipadds controlled components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,26 +1,76 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
+import { navigate } from "gatsby-link"
 
 
 const Contact = () => {
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        message: "",
+    })
+
+    const encode = (data) => {
+        return Object.keys(data).map(key => (
+            `${encodeURIComponent(key)}${encodeURIComponent(data[key])}`
+        )).join('&')
+    }
+
+    const handleChnage = (e) => {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await fetch('/contact', {
+            method: 'POST',
+            header: {
+                'Content-type': 'application/x-www-form-urlencoded'
+            },
+            body: encode(formState)
+        })
+        setFormState({
+            name: "",
+            email: "",
+            message: "",
+        })
+        navigate('/')
+    }
     return (
         <Layout>
             <Head page="Contact" />
             <h1>Contact</h1>
-            <form name="contact" method="POST" data-netlify="ture">
-                <input type="hidden"  name="form-name" value="contact" />
+            <form name="contact"  data-netlify="ture" onSubmit={handleSubmit}>
+                <input 
+                    type="hidden"
+                    name="form-name" 
+                    value="contact" />
                 <label>
                     Name
-                    <input type="text" name="name" placeholder="First Name  Last Name" />
+                    <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="First Name  Last Name"
+                        onChange={handleChnage}
+                        value={formState.name} />
                 </label>
                 <label>
                     Email
-                    <input type="emial" name="email" placeholder="Email Address" />
+                    <input 
+                        type="emial" 
+                        name="email" 
+                        placeholder="Email Address"
+                        onChange={handleChnage}
+                        value={formState.email} />
                 </label>
                 <label>
                     Message
-                    <textarea name="message" placeholder="Hey waht's up">
+                    <textarea 
+                        name="message" 
+                        placeholder="Hey waht's up"
+                        onChange={handleChnage}
+                        value={formState.message}>
 
                     </textarea>
                 </label>

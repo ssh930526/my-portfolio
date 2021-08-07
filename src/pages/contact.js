a8556461987baf170c54c100452c87415022cf08
Sihui Shen <sihui@Sihuis-MacBook-Pro.local>
2021-08-07 14:52:08 -0500

    wipfix the contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
+import Head from "../components/head"
 
 
 const Contact = () => {
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,31 +1,32 @@
 import React from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
-import data from "../data/projects.json"
-// import { StaticImage } from "gatsby-plugin-image"
+import projects from "../data/projects.json"
 
 
 const Projects = () => {
-    console.log(data)
   return (
     <Layout>
       <Head page="Projects" />
-      <h1>Projects Page</h1>
-      <section>
+      <section style={{display: "flex", alignItems: "center", justifyContent: "space-evenly"}}>
           {
-              data.map((project, idx) => (
-                <article key={idx}>
-                     <img style={{height: 175, border: "2px solid #333"}} src={project.image} alt={project.title} />
-                    <h2>{project.title}</h2>
-                    <p>{project.description}</p>
-                    <a href={project.githubLink}>View On GitHub</a>
-                    <br />
-                    <a href={project.deployedLink}>View Deployed Project</a>
-                </article>
-              ))
-          }
+              projects.map((project, idx) => {
+                  return (
+                    <article key={idx}>
+                        <img style={{height: 175, border: "2px solid #333"}} src={project.image} alt={project.title} />
+                        <h2>{project.title}</h2>
+                        <p>{project.description}</p>
+                        <a href={project.githubLink}>View On Github</a>
+                        <br />
+                        <a href={project.deployedLink}>View Deployed Project</a>
+                    </article>
+                 )
+                }
+            )
+          } 
       </section>
     </Layout>
   )
 }
+
 export default Projects
